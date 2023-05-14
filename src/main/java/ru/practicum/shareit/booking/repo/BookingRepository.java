10697ee7aded5d6d4e88389e0a13948d9ba01647
struuunks <withoutbrains1@yandex.ru>
2023-05-14 21:15:30 +0200

    ТЗ 14

--- src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findBookingsByStatusAndStartIsAfterOrderByStartAsc(Status status, LocalDateTime ldt);
+
     List<Booking> findBookingsByStatusAndEndIsBeforeOrderByStartDesc(Status status, LocalDateTime ldt);
 
     List<Booking> findBookingsByItemOwnerId(Long ownerId);
